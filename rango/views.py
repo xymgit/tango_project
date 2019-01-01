89aa0eeab45c97c2ec904579f19d58a6640aa76b
xymgit <xymgit@outlook.com>
2019-01-01 21:19:52 +0800

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 from rango.models import Category, Page
@@ -12,6 +13,42 @@ from rango.forms import UserForm, UserProfileForm
 
 # Create your views here.
 
+def get_server_side_cookie(request, cookie, default_vla=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_vla
+    return val
+
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    # visits = int(request.COOKIES.get('visits', '1'))
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    # last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # if it's been more than a day since the last visit...
+    # if (datetime.now() - last_visit_time).days > 0:
+    if (datetime.now() - last_visit_time).seconds > 5:
+        visits = visits + 1
+        # update the last visit cookie now that we have updated the count
+        # response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie
+        # response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    # response.set_cookie('visits', visits)
+    request.session['visits'] = visits
+
 
 def index(request):
     # return HttpResponse("Rango says hey there partner!<br/> <a href='/rango/about/'>About</a>")
@@ -19,16 +56,36 @@ def index(request):
     # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     # return render(request, 'rango/index.html', context=context_dict)
 
+    # request.session.set_test_cookie()
+
     Category_list = Category.objects.order_by('-likes')[:5]
     Page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': Category_list, 'pages': Page_list}
-    return render(request, 'rango/index.html', context_dict)
+    # return render(request, 'rango/index.html', context_dict)
+
+    # Obtain our Response object early so we can add cookie information.
+    # response = render(request, 'rango/index.html', context_dict)
+
+    # Call function to handle the cookies
+    # visitor_cookie_handler(request, response)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context_dict)
+    
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 
 
 def about(request):
     # To complete the exercise in chapter 4, we need to remove the following line
     # return HttpResponse("Rango says here is the about page.<br/> <a href='/rango/'>View index page</a>")
     # and replace it with a pointer to ther about.html template using the render method
+
+    # if request.session.test_cookie_worked():
+    #     print("TEST COOKIE WORKED!")
+    #     request.session.delete_test_cookie()
+
     return render(request, 'rango/about.html', {})
 
 
